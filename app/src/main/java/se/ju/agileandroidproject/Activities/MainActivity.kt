d7d83d350e8e78a0cc927f56cc5d4ea626812f6e
Marcus Gullstrand <guma1502@student.ju.se>
2019-04-10 15:00:00 +0200

    Test

--- app/src/main/java/se/ju/agileandroidproject/Activities/MainActivity.kt
+++ app/src/main/java/se/ju/agileandroidproject/Activities/MainActivity.kt
@@ -1,7 +1,11 @@
 package se.ju.agileandroidproject.Activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import se.ju.agileandroidproject.GPSHandler
 import se.ju.agileandroidproject.Models.Gantry
 import se.ju.agileandroidproject.Models.Invoice
@@ -15,8 +19,45 @@ class MainActivity : AppCompatActivity() {
 
     private val invoice = Invoice()
 
+    private fun checkPermissions() {
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR)
+//            != PackageManager.PERMISSION_GRANTED) {
+//            // Permission is not granted
+//        }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.INTERNET)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.INTERNET),
+                    1)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
     }
 }
