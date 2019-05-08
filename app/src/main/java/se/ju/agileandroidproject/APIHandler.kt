ce6620eefe28a58870f2bab0dd75a7f60c9bff19
Filip Andersson <filipandersson.1997.245@gmail.com>
2019-05-08 16:49:37 +0200

    Adds signin/register with respective testsuite.

--- app/src/main/java/se/ju/agileandroidproject/APIHandler.kt
+++ app/src/main/java/se/ju/agileandroidproject/APIHandler.kt
@@ -4,28 +4,35 @@ import com.github.kittinunf.fuel.*
 import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
+import com.github.kittinunf.result.Result
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.stringify
 
 import se.ju.agileandroidproject.Models.Gantry
+import se.ju.agileandroidproject.Models.Session
+import se.ju.agileandroidproject.Models.User
 
+@UnstableDefault
+@ImplicitReflectionSerializer
 object APIHandler {
 
-    val url = "http://agileserver-env.yttgtpappn.eu-central-1.elasticbeanstalk.com"
-    val token = "fhsakdjhjkfds"
+    private const val url = "http://agileserver-env.yttgtpappn.eu-central-1.elasticbeanstalk.com"
+    var token = ""
 
-    @UnstableDefault
-    suspend fun returnGantry(lon : Float, lat : Float) : List<Gantry> {
+    suspend fun returnGantry(lon: Float, lat: Float): List<Gantry> {
 
-        val (request, response, result) =
-            Fuel.post(url + "/gantries/" + "abc123")
+        val (_, _, result) =
+            Fuel.post("$url/gantries/abc123")
                 .authentication()
                 .bearer(token)
                 .jsonBody("{ \"position\": [3.213134, 12.438324] }")
                 .awaitStringResponseResult()
 
 
-        var responseData = mutableListOf<Gantry>()
+        val responseData = mutableListOf<Gantry>()
 
         result.fold(
             { data ->
@@ -36,5 +43,58 @@ object APIHandler {
             })
 
         return responseData
+    }
+
+    fun loginRequest(username: String, password: String): Session {
+        val (_, _, result) = runBlocking {
+            Fuel.post("$url/sessions/")
+                .jsonBody("{ \"username\": \"$username\", \"password\": \"$password\", \"grant_type\": \"password\" }")
+                .awaitStringResponseResult()
+        }
+
+        var token = Session(false, "")
+        result.fold({ data ->
+            token = Json.parse(Session.serializer(), data)
+        }, {
+            print("Failed with login attempt.")
+        })
+
+        return token
+    }
+
+    fun login(username: String, password: String): Boolean {
+        val session = loginRequest(username, password)
+        return when (session.auth) {
+            true -> {
+                this.token = session.token
+                true
+            }
+            else -> {
+                token = ""
+                false
+            }
+        }
+    }
+
+    fun login(user: User): Boolean = login(user.personalIdNumber, user.password)
+
+    fun registerRequest(user: User): Boolean {
+        val (_, _, result) = runBlocking {
+            Fuel.post("$url/user/")
+                .jsonBody(Json.stringify(user))
+                .awaitStringResponseResult()
+        }
+        return result.fold({ true }, { error -> print(error.message); false })
+    }
+
+    fun register(user: User): Boolean {
+        return when (registerRequest(user)) {
+            true -> {
+                login(user)
+            }
+            else -> {
+                print("Registration failed!"); false
+            }
+        }
     }
 }
